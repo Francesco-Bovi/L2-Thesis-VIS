04e2e569f18a39665a3fc0d720f17f1d52e3a052
Francesco-Bovi <bovi.1803762@studenti.uniroma1.it>
2022-03-30 15:32:00 +0200

    building graph-add transition v1

--- graphbuilder_v2.py
+++ graphbuilder_v2.py
@@ -1,7 +1,9 @@
 import json
+from textwrap import indent
 
 graph={}
 
+""""
 def add_node_parent(s):
     global graph
     #print("----Adding node:",s,"----")
@@ -9,15 +11,29 @@ def add_node_parent(s):
         graph[s+"_hover"]=[]
     else:
         graph[s]=[]
+"""
 
 def add_state(s):
     global graph
     #print("----Adding node:",s,"----")
     graph[s]=[]
 
-def traverse_json(node):
+def add_transition(transitions,parent):
+    global graph
+    print("PARENT-",parent)
+    for t in transitions:
+        print("DEBUG ACTION -",t,"-",transitions.get(t))
+        target_array=transitions.get(t)
+        for elem in target_array:
+            print("TARGET-->",elem['target'])
+            transition_array=[t,elem['target']]
+            graph[parent].append(transition_array)
+
+def traverse_json(node,parent):
     global states_aux
     if(node.get('states') is None):
+        if(node.get('on') is not None):
+            add_transition(node.get('on'),parent)
         return
     else:
         #If is not a parent node add it
@@ -25,7 +41,7 @@ def traverse_json(node):
             add_state(node.get('id'))
         #Explore other childs
         for i in node.get('states'):
-            traverse_json(node.get('states').get(i))
+            traverse_json(node.get('states').get(i),node.get('id'))
 
 #open the statechart json file
 statechart_j=open('xstate_visualization_statechart.json')
@@ -44,10 +60,12 @@ for i in statechart_dict['states']:
     states_aux.append(i)
 
     #Add principal states with hovering
-    add_node_parent(i)
+    add_state(i)
 
     #Add child states
-    traverse_json(statechart_dict.get('states').get(i))
+    traverse_json(statechart_dict.get('states').get(i),i)
 
 #Print graph representation
+print("----------------------------")
 print("Graph representation: ",graph)
+print("Graph:",json.dumps(graph,indent=4))
