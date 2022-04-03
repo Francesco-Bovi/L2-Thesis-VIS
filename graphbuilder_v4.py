26af74eb2350586454626ba334d8a2fcb45f78db
Francesco-Bovi <bovi.1803762@studenti.uniroma1.it>
2022-04-03 12:28:37 +0200

    Explore graph seems to go well v4

--- graphbuilder_v4.py
+++ graphbuilder_v4.py
@@ -1,5 +1,6 @@
 import json
-from turtle import st
+from queue import Empty
+import random
 
 
 class StateMachine:
@@ -12,19 +13,9 @@ class Container:
         self.ID = ID
         self.Transitions  = []
         self.Subsets = []
-        self.Parent = None 
+        self.Parent = None
 
-"""
-class State:
-    def __init__(self, Contenuto=None, ProssimoNodo=None):
-        self.Contenuto = Contenuto
-        self.ProssimoNodo  = ProssimoNodo
-    
-    def __str__(self):
-        return str(self.Contenuto)
-"""
-
-def create_graph(states,container):
+def CreateGraph(states,container):
     for child in states:
         
         new_container=Container(child)
@@ -32,16 +23,16 @@ def create_graph(states,container):
         #print(new_container.ID)
 
         if(states.get(child).get("on") is not None):
-            new_container.Transitions=add_transitions(states.get(child).get('on'))
+            new_container.Transitions=AddTransitions(states.get(child).get('on'))
 
         container.Subsets.append(new_container)
         #print(container)
 
         #Explore rest of graph
         if(states.get(child).get("states") is not None):
-            create_graph(states.get(child).get("states"),new_container)
+            CreateGraph(states.get(child).get("states"),new_container)
 
-def add_transitions(transitions):
+def AddTransitions(transitions):
     transition_array=[]
     for t in transitions:
         target_array=transitions.get(t)
@@ -58,6 +49,121 @@ def PrintGraph(state,level=0):
     for item in state.Subsets:
         PrintGraph(item,level+1)
 
+def PrintNode(state,level=1):
+    print(state.ID)
+    if(level!=0):
+        print("PARENT:", state.Parent.ID)
+    print("TRANSITIONS",state.Transitions)
+    print("SUBSET:")
+    print("\t",state.Subsets)
+    for item in state.Subsets:
+        print("\t",item.ID)
+
+def findTransaction(state,id):
+    for i in state.Subsets:
+        if(i.ID==id):
+            return i
+
+def ExploreGraph(state):
+    PrintNode(state,0)
+
+    #Possible next states
+    substates=state.Subsets
+    transactions=state.Transitions
+    parent=state.Parent
+
+    #In each state I can go inside, at same level or at the parent
+    start=random.randint(0,2)
+
+    if(start==0):
+
+        if(substates!=[]):
+            #Explore a random state
+            start=random.randint(0,len(substates)-1)
+            next_state=substates[start]
+            
+            #Print the state
+            print("SUBSTATE")
+            print("NEXT_STATE->",next_state.ID)
+            ExploreGraph(next_state)
+        
+        elif(transactions!=[]):
+            #Explore a random state
+            start=random.randint(0,len(transactions)-1)
+            next_state_id=transactions[start][1]
+            next_state=findTransaction(parent,next_state_id)
+
+            #Print the state
+            print("TRANSACTIONS")
+            print("NEXT_STATE->",next_state.ID)
+            ExploreGraph(next_state)
+
+        else:
+            next_state=state.Parent
+            #Print the state
+            print("PARENT")
+            print("NEXT_STATE->",next_state.ID)
+            ExploreGraph(next_state)          
+    
+    elif(start==1):
+
+        if(transactions!=[]):
+
+            #Explore a random state
+            start=random.randint(0,len(transactions)-1)
+            next_state_id=transactions[start][1]
+            next_state=findTransaction(parent,next_state_id)
+            
+            #Print the state
+            print("TRANSACTIONS")
+            print("NEXT_STATE->",next_state.ID)
+            ExploreGraph(next_state)
+
+        elif(substates!=[]):
+            #Explore a random state
+            start=random.randint(0,len(substates)-1)
+            next_state=substates[start]
+            
+            #Print the state
+            print("SUBSTATE")
+            print("NEXT_STATE->",next_state.ID)
+            ExploreGraph(next_state)
+        else:
+            next_state=state.Parent
+            #Print the state
+            print("PARENT")
+            print("NEXT_STATE->",next_state.ID)
+            ExploreGraph(next_state)
+    
+    else:
+        if(parent!=None):
+            next_state=state.Parent
+            #Print the state
+            print("PARENT")
+            print("NEXT_STATE->",next_state.ID)
+            ExploreGraph(next_state)
+
+        elif(transactions!=[]):
+    
+            #Explore a random state
+            start=random.randint(0,len(transactions)-1)
+            next_state_id=transactions[start][1]
+            next_state=findTransaction(parent,next_state_id)
+            
+            #Print the state
+            print("TRANSACTIONS")
+            print("NEXT_STATE->",next_state.ID)
+            ExploreGraph(next_state)
+        
+        elif(substates!=[]):
+            #Explore a random state
+            start=random.randint(0,len(substates)-1)
+            next_state=substates[start]
+            
+            #Print the state
+            print("SUBSTATE")
+            print("NEXT_STATE->",next_state.ID)
+            ExploreGraph(next_state)
 
 if(__name__=="__main__"):
 
@@ -76,17 +182,15 @@ if(__name__=="__main__"):
     root_node=Container("vis")
     state_machine=StateMachine(root_node)
 
-    root_node.Parent=state_machine
+    #root_node.Parent=state_machine
 
-    create_graph(statechart_dict.get('states'),root_node)
+    CreateGraph(statechart_dict.get('states'),root_node)
    
     #root_container.Subsets.append(Container("scatter"))
     #root_container.Subsets.append(Container("barchart"))
 
     print("----------------------------")
     print("Graph representation: ")
-    print("VIS: ")
-    print("TRANSITIONS:",root_node.Subsets)
+    #PrintGraph(root_node)
 
-    print("----PRINT----")
-    PrintGraph(root_node)
+    ExploreGraph(root_node)
