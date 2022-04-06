837279ff3dcd7d2c536e09a73b2d5289ebd0fbf1
Francesco-Bovi <bovi.1803762@studenti.uniroma1.it>
2022-04-06 15:38:46 +0200

    comment added to graphbuilderv3_2

--- graphbuilder_v3_2.py
+++ graphbuilder_v3_2.py
@@ -2,24 +2,15 @@ import json
 import random
 from textwrap import indent
 
-def AddState(state,graph,parent=None):
 
-    #Check if already exists a ley with that value
-    if(parent!=None):
-        sub_dict=[]
-        graph[state+"_"+parent]=sub_dict
+#Function used to add a key in the dictionary
+def AddState(state,graph,parent):
 
-        return state+"_"+parent
+    #Crete a key in the dictionary based on the parent
+    sub_dict=[]
+    graph[state+"_"+parent]=sub_dict
 
-    else:
-
-        sub_dict=[]
-        graph[state]=sub_dict
-
-        return state
-
-
-    #print("Graph representation: ",graph)
+    return state+"_"+parent
 
 
 def AddTransitions(transitions,parent=None,child=None):
@@ -28,20 +19,24 @@ def AddTransitions(transitions,parent=None,child=None):
         target_array=transitions.get(t)
         for elem in target_array:
 
-            #Reference to parent if we found a transition to hover
+            #Reference to parent if we found a transition to hover or idle
             if(elem["target"]=="hover" or elem["target"]=="idle"):
                 if(parent!=None):
                     transition_array.append([t,parent])
             else:
+
+                #If is a substate
                 if(parent!=None):
                     transition_array.append([t,elem["target"]+"_"+parent])
+                
+                #If is not a substate
                 else:
                     transition_array.append([t,elem["target"]+"_"+child])
 
     return transition_array
 
 #Function to build the graph
-def create_graph(states,graph,parent_tranistions=None,parent=None):
+def CreateGraph(states,graph,parent_tranistions=None,parent=None):
     transitions=[]
     for child in states:
         print("CHILD:",child)
@@ -51,11 +46,13 @@ def create_graph(states,graph,parent_tranistions=None,parent=None):
         #We are not interested in hover or idle since we have considered them previoulsy
         if(child!="hover" and child!="idle"):
 
-            #Add state checking if it's a real state
+            #Case when it's not a Container state
             if(states.get(child).get("initial") is None):
 
+                #Add the new key to the graph using the name of the parent (Container)
                 child_name=AddState(child,graph,parent)
 
+                #Check if it has transitions (and add them)
                 if(states.get(child).get("on") is not None):
                     transitions=AddTransitions(states.get(child).get("on"),parent,child_name)
                     print(transitions)
@@ -69,6 +66,8 @@ def create_graph(states,graph,parent_tranistions=None,parent=None):
 
                 print("TRANSITIONS:",transitions)
 
+                #Add parent transitions to the transitions of the new state
+                #Those will be inherited by the substates
                 if(parent_tranistions!=None):
                     for t in parent_tranistions:
                         transitions.append(t)
@@ -82,20 +81,28 @@ def create_graph(states,graph,parent_tranistions=None,parent=None):
 
                         graph[child_name].append(["MOUSEMOVE",node+"_"+child_name])
 
+                #Go inside the substates
                 if(states.get(child).get("states") is not None):
-                    create_graph(states.get(child).get("states"),graph,transitions,child_name)
+                    CreateGraph(states.get(child).get("states"),graph,transitions,child_name)
             
-            #Case when the state is a container
+            #Case when the state is a Container
             else:
+
+                #Name of the default initial state
                 initial_state=states.get(child).get("initial")
+
+                #Add the new key to the graph using the name of the parent (Container)
                 child_name=AddState(child,graph,parent)
 
+                #Check if it has transitions (and add them)
                 if(states.get(child).get("on") is not None):
                     transitions=AddTransitions(states.get(child).get("on"),parent,child_name)
                     print(transitions)
                     for t in transitions:
                         graph[child_name].append(t)
 
+                #In this case add also the transitions of the ipotetical initial state
+                #Since we consider the actual state having the same transitions of its initial one
                 if(states.get(child).get("states").get(initial_state).get("on") is not None):
                     transition_initial_state=AddTransitions(states.get(child).get("states").get(initial_state).get("on"),child_name,child_name)
                     for t in transition_initial_state:
@@ -108,6 +115,8 @@ def create_graph(states,graph,parent_tranistions=None,parent=None):
         
                 print("TRANSITIONS:",transitions)
 
+                #Add parent transitions to the transitions of the new state
+                #Those will be inherited by the substates
                 if(parent_tranistions!=None):
                     for t in parent_tranistions:
                         transitions.append(t)
@@ -121,9 +130,12 @@ def create_graph(states,graph,parent_tranistions=None,parent=None):
 
                         graph[child_name].append(["MOUSEMOVE",node+"_"+child_name])
                 
+                #Go inside the substates
                 if(states.get(child).get("states") is not None):
-                    create_graph(states.get(child).get("states"),graph,transitions,child_name)
+                    CreateGraph(states.get(child).get("states"),graph,transitions,child_name)
 
+        #If we are in "hover" or "idle" we don't consider them because we have done it previously
+        #So we just skip
         else:
 
             print("------------------------------------------------------------------------------")
@@ -158,21 +170,27 @@ def ExploreGraph(graph,graph_s,state):
     #Check if we have vistied all states at least once
     if not CheckScore(graph_s):
 
+        #List of all possible transitions from the current state
         list_possible_transitions=graph[state]
         len_list=len(list_possible_transitions)-1
 
+        #Randon number for the possible next state (if all have been visited yet)
         ran_number=random.randint(0,len_list)
 
         next_transaction=ChooseNextState(graph_s,ran_number,list_possible_transitions)
         #print("NEXT TRANSITION:",next_transaction[0]," TO:",next_transaction[1])
 
+        #Continue the exploration
         ExploreGraph(graph,graph_s,next_transaction[1])
     
     else:
+
+        #If all states have been visted at least once stop exploring the dictionary
         return
 
 
 if(__name__=="__main__"):
+
     #open the statechart json file
     statechart_j=open('xstate_visualization_statechart_aux.json')
 
@@ -182,13 +200,12 @@ if(__name__=="__main__"):
     #Data structure for that will contain the graph
     graph={}
 
-    create_graph(statechart_dict.get('states'),graph,None,"vis")
+    CreateGraph(statechart_dict.get('states'),graph,None,"vis")
 
     #Print graph representation
     print("------------------------------------------------------------------------------")
     print("------------------------------------------------------------------------------")
-    #print("Graph representation: ",graph)
-    #print("Graph:",json.dumps(graph,indent=4))
+    print("Graph:",json.dumps(graph,indent=4))
 
     #Save graph on a file
     with open('statechartv3_2.json', 'w') as fp:
@@ -202,15 +219,17 @@ if(__name__=="__main__"):
     #Print graph with scores
     print(graph_score)
 
+    #Choosa randomly the first state to visit
     start=random.randint(0,len(statechart_dict.get('states'))-1)
-    print(statechart_dict.get('states'))
     list_graph=list(statechart_dict.get('states'))
 
+    #Print inital state
     initial_state=list_graph[start]+"_vis"
     print("FIRST STATE:",initial_state)
 
+    #Start the exploration of the graph
     ExploreGraph(graph,graph_score,initial_state)
 
     print("------------------------------------------------------------------------------")
-    print("All states visited at least once")
     print("Score Graph:",json.dumps(graph_score,indent=4))
+    print("All states visited at least once")
